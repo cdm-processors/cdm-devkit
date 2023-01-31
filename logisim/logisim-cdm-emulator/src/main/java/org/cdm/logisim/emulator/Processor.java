3dbc7853b5c233519e712ab2fbcd0f4272a5e37e
miqqra <mikhail.krasnikov.04@gmail.com>
2023-01-31 21:26:36 +0700

    [logisim-cdm-emulator] Change structure of Processor and ProcessorComponent

--- logisim/logisim-cdm-emulator/src/main/java/org/cdm/logisim/emulator/Processor.java
+++ logisim/logisim-cdm-emulator/src/main/java/org/cdm/logisim/emulator/Processor.java
@@ -16,56 +16,9 @@ import com.cburch.logisim.util.StringUtil;
 import java.awt.*;
 
 public class Processor {
-    private static final LocaleManager source = new LocaleManager("resources/logisim", "std");
 
-    public static StringGetter getter(String key) {
-        return source.getter(key);
-    }
-
-    public static String get(String key) {
-        return source.get(key);
-    }
-
-    public static String get(String key, String arg0) {
-        return StringUtil.format(source.get(key), arg0);
-    }
-
-    public static String get(String key, String arg0, String arg1) {
-        return StringUtil.format(source.get(key), arg0, arg1);
-    }
     public Processor() {
-        super("Processor");
-        Port[] ps = new Port[]{
-                new Port(0, 0, "input", 16),
-                new Port(10, 0, "output", 16),
-                new Port(20, 0, "output", 16),
-                new Port(30, 0, "output", 1),
-                new Port(40, 0, "output", 1),
-                new Port(50, 0, "output", 1),
-                new Port(60, 0, "output", 1),
-                new Port(70, 0, "input", 1),
-                new Port(80, 0, "input", 5),
-                new Port(90, 0, "input", 1),
-                new Port(100, 0, "input", 5),
-                new Port(110, 0, "input", 1),
-                new Port(120, 0, "input", 1),
-                new Port(120, 0, "output", 1)};
 
-        ps[0].setToolTip(getter("dataIn"));
-        ps[1].setToolTip(getter("dataOut"));
-        ps[2].setToolTip(getter("address"));
-        ps[3].setToolTip(getter("mem"));
-        ps[4].setToolTip(getter("data"));
-        ps[5].setToolTip(getter("read"));
-        ps[6].setToolTip(getter("word"));
-        ps[7].setToolTip(getter("irq"));
-        ps[8].setToolTip(getter("intNumber"));
-        ps[9].setToolTip(getter("exc"));
-        ps[10].setToolTip(getter("clk"));
-        ps[11].setToolTip(getter("excNumber"));
-        ps[12].setToolTip(getter("hold/wait"));
-        ps[13].setToolTip(getter("halted"));
-        this.setPorts(ps);
     }
 
     public void externalInterrupt(int interruptNumber) {
@@ -88,104 +41,8 @@ public class Processor {
 
     }
 
-    @Override
-    public void paintInstance(InstancePainter painter) {
-        Graphics g = painter.getGraphics();
-        Bounds bds = painter.getBounds();
-        painter.drawBounds();
-
-        // draw contents
-        if (painter.getShowState()) {
-            MemState state = getState(painter);
-            state.paint(painter.getGraphics(), bds.getX(), bds.getY());
-        } else {
-            BitWidth addr = painter.getAttributeValue(ADDR_ATTR);
-            int addrBits = addr.getWidth();
-            int bytes = 1 << addrBits;
-            String label;
-            if (this instanceof BankedROM) {
-
-                if (addrBits >= 30) {
-                    label = StringUtil.format(get("romGigabyteLabel"), ""
-                            + (bytes >>> 30));
-                } else if (addrBits >= 20) {
-                    label = StringUtil.format(get("romMegabyteLabel"), ""
-                            + (bytes >> 20));
-                } else if (addrBits >= 10) {
-                    label = StringUtil.format(get("romKilobyteLabel"), ""
-                            + (bytes >> 10));
-                } else {
-                    label = StringUtil.format(get("romByteLabel"), ""
-                            + bytes);
-                }
-            } else {
-                if (addrBits >= 30) {
-                    label = StringUtil.format(get("ramGigabyteLabel"), ""
-                            + (bytes >>> 30));
-                } else if (addrBits >= 20) {
-                    label = StringUtil.format(get("ramMegabyteLabel"), ""
-                            + (bytes >> 20));
-                } else if (addrBits >= 10) {
-                    label = StringUtil.format(get("ramKilobyteLabel"), ""
-                            + (bytes >> 10));
-                } else {
-                    label = StringUtil.format(get("ramByteLabel"), ""
-                            + bytes);
-                }
-            }
-            GraphicsUtil.drawCenteredText(g, label, bds.getX() + bds.getWidth()
-                    / 2, bds.getY() + bds.getHeight() / 2);
-        }
-
-        // draw input and output ports
-        painter.drawPort(DATA, BankedStrings.get("ramDataLabel"), Direction.WEST);
-        painter.drawPort(ADDR, BankedStrings.get("ramAddrLabel"), Direction.EAST);
-        g.setColor(Color.GRAY);
-        painter.drawPort(CS, BankedStrings.get("ramCSLabel"), Direction.SOUTH);
-
-        if (this instanceof BankedROM) {
-            painter.drawPort(BankedROM.BITS, BankedStrings.get("bit"), Direction.SOUTH);
-        } else if (this instanceof BankedRAM) {
-            painter.drawPort(BankedRAM.BITS, BankedStrings.get("bit"), Direction.SOUTH);
-        }
-
+    public enum ClockType {
+        EXC, CLK, IRQ
     }
 
-    @Override
-    public void propagate(InstanceState state) {
-        ProcessorClockState data = (ProcessorClockState) state.getData();
-        if (data == null) {
-            data = new ProcessorClockState();
-            state.setData(data);
-        }
-
-        Object irqTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
-        boolean irqTriggered = data.updateClock(state.getPort(7), irqTriggerType, ClockType.IRQ);
-
-        Object excTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
-        boolean excTriggered = data.updateClock(state.getPort(9), excTriggerType, ClockType.EXC);
-
-        Object clkTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
-        boolean clkTriggered = data.updateClock(state.getPort(10), clkTriggerType, ClockType.CLK);
-
-
-        if (irqTriggered){
-            externalInterrupt(state.getPort(8).toIntValue());
-        }
-        if (excTriggered){
-            externalException(state.getPort(11).toIntValue());
-        }
-        if (clkTriggered){
-            clockRising();
-        }
-        else {
-            clockFalling();
-        }
-
-        update();
-    }
-
-    public enum ClockType{
-        IRQ, EXC, CLK
-    }
 }
--- logisim/logisim-cdm-emulator/src/main/java/org/cdm/logisim/emulator/ProcessorComponent.java
+++ logisim/logisim-cdm-emulator/src/main/java/org/cdm/logisim/emulator/ProcessorComponent.java
@@ -1,129 +1,167 @@
 package org.cdm.logisim.emulator;
 
-import com.cburch.logisim.data.Attribute;
 import com.cburch.logisim.data.BitWidth;
 import com.cburch.logisim.data.Bounds;
-import com.cburch.logisim.data.Value;
+import com.cburch.logisim.data.Direction;
 import com.cburch.logisim.instance.InstanceFactory;
 import com.cburch.logisim.instance.InstancePainter;
 import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.instance.Port;
 import com.cburch.logisim.instance.StdAttr;
+import com.cburch.logisim.util.GraphicsUtil;
+import com.cburch.logisim.util.LocaleManager;
+import com.cburch.logisim.util.StringGetter;
+import com.cburch.logisim.util.StringUtil;
 
-/** Этот компонент принимает многобитное значение на входе и выдаёт значение, следующее за ним
- * в коде Грея. Например, при входном значении 0100 на выходе будет 1100. */
 class ProcessorComponent extends InstanceFactory {
-    /* Обратите внимание на то, что нет переменных экземпляра. Только один экземпляр
-     * создан этим классом, и он управляет всеми экземплярами компонента. С любой
-     * информацией, связанной с отдельными экземплярами нужно работать
-     * через атрибуты. Для GrayIncrementer каждый экземпляр имеет "разрядность",
-     * с которой он работает, так что у нас будет атрибут. */
+    private static final LocaleManager source = new LocaleManager("resources/logisim", "std");
 
-    /** Конструктор конфигурирует фабрику. */
+    public static StringGetter getter(String key) {
+        return source.getter(key);
+    }
+
+    public static String get(String key) {
+        return source.get(key);
+    }
+
+    public static String get(String key, String arg0) {
+        return StringUtil.format(source.get(key), arg0);
+    }
+
+    public static String get(String key, String arg0, String arg1) {
+        return StringUtil.format(source.get(key), arg0, arg1);
+    }
     ProcessorComponent() {
-        super("Gray Code Incrementer");
+        super("ProcessorComponent");
 
-        /* Так мы можем устанавливать атрибуты для GrayIncrementer'ов. В
-         * этом случае атрибут только один - разрядность - и он по умолчанию
-         * равен 4. Класс StdAttr объявляет некоторые часто встречающиеся
-         * атрибуты, включая "разрядность". Лучше использовать эти
-         * атрибуты StdAttr, когда это уместно: пользователь может выбрать несколько
-         * компонентов (даже из разных фабрик) с одинаковым атрибутом
-         * и изменять их все сразу. */
-        setAttributes(new Attribute[] { StdAttr.WIDTH },
-                new Object[] { BitWidth.create(4) });
+        //setOffsetBounds(Bounds.create(-30, -15, 30, 30));
 
-        /* "Offset bounds" - это положение ограничивающего прямоугольника
-         * относительно положения мыши. Здесь мы задаём компонент
-         * 30x30, и закрепляем его относительно его основного выхода
-         * (это типично для Logisim), расположенного в центре
-         * восточного края. Так, левый верхний угол ограничивающего прямоугольника на 30 пикселей
-         * западнее и на 15 пикселей севернее положения мыши. */
-        setOffsetBounds(Bounds.create(-30, -15, 30, 30));
+        Port[] ps = new Port[]{
+                new Port(0, 0, "input", 16),
+                new Port(10, 0, "output", 16),
+                new Port(20, 0, "output", 16),
+                new Port(30, 0, "output", 1),
+                new Port(40, 0, "output", 1),
+                new Port(50, 0, "output", 1),
+                new Port(60, 0, "output", 1),
+                new Port(70, 0, "input", 1),
+                new Port(80, 0, "input", 5),
+                new Port(90, 0, "input", 1),
+                new Port(100, 0, "input", 5),
+                new Port(110, 0, "input", 1),
+                new Port(120, 0, "input", 1),
+                new Port(120, 0, "output", 1)};
 
-        /* Порты - это места, где провода могут быть присоединены к этому
-         * компоненту. Каждый объект Port говорит, где искать порт относительно
-         * места крепления компонента, затем является ли порт
-         * входом/выходом/обоими, и наконец ожидаемую разрядность для порта.
-         * Разрядность может быть константой (например, 1) или атрибутом (как здесь).
-         */
-
-        /*
-        data_in - input
-data_out - output
-address - output
-mem - output
-data - output
-read - output
-word - output
-irq - input, invoke externalInterrupt(int interruptNumber) on rising edge, it's argument is read from int_number input
-int_number - input
-exc - input, invoke externalException(int exceptionNumber) on rising edge, it's argument is read from exc_number input
-exc_number - input
-clk - input, invoke clockRising() on rising edge, clockFalling() on falling edge
-hold/wait - input, TBD
-halted - output
-         */
-        setPorts(new Port[] {
-                new Port(-30, 0, Port.INPUT, StdAttr.WIDTH),
-                new Port(0, 0, Port.OUTPUT, StdAttr.WIDTH),
-
-        });
+        ps[0].setToolTip(getter("dataIn"));
+        ps[1].setToolTip(getter("dataOut"));
+        ps[2].setToolTip(getter("address"));
+        ps[3].setToolTip(getter("mem"));
+        ps[4].setToolTip(getter("data"));
+        ps[5].setToolTip(getter("read"));
+        ps[6].setToolTip(getter("word"));
+        ps[7].setToolTip(getter("irq"));
+        ps[8].setToolTip(getter("intNumber"));
+        ps[9].setToolTip(getter("exc"));
+        ps[10].setToolTip(getter("clk"));
+        ps[11].setToolTip(getter("excNumber"));
+        ps[12].setToolTip(getter("hold/wait"));
+        ps[13].setToolTip(getter("halted"));
+        this.setPorts(ps);
     }
 
-    /** Вычисляет текущее значение на выходе для этого компонента. Этот метод вызывается
-     * каждый раз, когда значение на любом из входов меняется; он также может быть вызван при
-     * других обстоятельствах, даже если нет никаких оснований ожидать, что он изменит
-     * что-то. */
-    public void propagate(InstanceState state) {
-
-        // Сначала мы получаем значение, поданное на вход. Отметим, что при
-        // вызове setPorts выше, вход компонента был включен
-        // с индексом 0 в массив-параметр, так что мы используем 0 как параметр ниже.
-        Value in = state.getPort(0);
-
-        // Теперь вычислим значение на выходе. Мы передали эту работу вспомогательному методу,
-        // раз уж такая логика приемлема для других библиотечных компонентов.
-        Value out = nextGray(in);
-
-        // Наконец, мы передаём значение с выхода дальше в схему. Первый параметр
-        // равен 1, потому что в нашем списке портов (сформированном вызовом
-        // setPorts выше) выход имеет индекс 1. Второй параметр - это
-        // значение, которое мы хотим послать на этот порт. И последний параметр - это его
-        // "задержка" - количество шагов, которое уйдёт на обновление выхода
-        // после поступления значения на вход.
-        state.setPort(1, out, out.getWidth() + 1);
-    }
-
-    /** Говорит, как должен отдельный экземпляр появляться на холсте. */
+    @Override
     public void paintInstance(InstancePainter painter) {
-        // Когда это происходит, InstancePainter содержит несколько удобных методов
-        // для отрисовки и мы будем использовать их здесь. Часто вам захочется
-        // получить его объект Graphics (painter.getGraphics), так что вы сможете рисовать
-        // непосредственно на холсте.
-        painter.drawRectangle(painter.getBounds(), "G+1");
-        painter.drawPorts();
+        Graphics g = painter.getGraphics();
+        Bounds bds = painter.getBounds();
+        painter.drawBounds();
+
+        if (painter.getShowState()) {
+            MemState state = getState(painter);
+            state.paint(painter.getGraphics(), bds.getX(), bds.getY());
+        } else {
+            BitWidth addr = painter.getAttributeValue(ADDR_ATTR);
+            int addrBits = addr.getWidth();
+            int bytes = 1 << addrBits;
+            String label;
+            if (this instanceof BankedROM) {
+
+                if (addrBits >= 30) {
+                    label = StringUtil.format(get("romGigabyteLabel"), ""
+                            + (bytes >>> 30));
+                } else if (addrBits >= 20) {
+                    label = StringUtil.format(get("romMegabyteLabel"), ""
+                            + (bytes >> 20));
+                } else if (addrBits >= 10) {
+                    label = StringUtil.format(get("romKilobyteLabel"), ""
+                            + (bytes >> 10));
+                } else {
+                    label = StringUtil.format(get("romByteLabel"), ""
+                            + bytes);
+                }
+            } else {
+                if (addrBits >= 30) {
+                    label = StringUtil.format(get("ramGigabyteLabel"), ""
+                            + (bytes >>> 30));
+                } else if (addrBits >= 20) {
+                    label = StringUtil.format(get("ramMegabyteLabel"), ""
+                            + (bytes >> 20));
+                } else if (addrBits >= 10) {
+                    label = StringUtil.format(get("ramKilobyteLabel"), ""
+                            + (bytes >> 10));
+                } else {
+                    label = StringUtil.format(get("ramByteLabel"), ""
+                            + bytes);
+                }
+            }
+            GraphicsUtil.drawCenteredText(g, label, bds.getX() + bds.getWidth()
+                    / 2, bds.getY() + bds.getHeight() / 2);
+        }
+
+        // draw input and output ports
+        painter.drawPort(DATA, BankedStrings.get("ramDataLabel"), Direction.WEST);
+        painter.drawPort(ADDR, BankedStrings.get("ramAddrLabel"), Direction.EAST);
+        g.setColor(Color.GRAY);
+        painter.drawPort(CS, BankedStrings.get("ramCSLabel"), Direction.SOUTH);
+
+        if (this instanceof BankedROM) {
+            painter.drawPort(BankedROM.BITS, BankedStrings.get("bit"), Direction.SOUTH);
+        } else if (this instanceof BankedRAM) {
+            painter.drawPort(BankedRAM.BITS, BankedStrings.get("bit"), Direction.SOUTH);
+        }
+
     }
 
-    /** Вычисляет следующее значение кода Грея в последовательности после предыдущего. Этот статический
-     * метод просто выполняет некоторые манипуляции с битами; ему не надо делать ничего особенного с
-     * Logisim, за исключением того, что он манипулирует с объектами Value и BitWidth. */
-    static Value nextGray(Value prev) {
-        BitWidth bits = prev.getBitWidth();
-        if(!prev.isFullyDefined()) return Value.createError(bits);
-        int x = prev.toIntValue();
-        int ct = (x >> 16) ^ x; // вычислить соотношение для x
-        ct = (ct >> 8) ^ ct;
-        ct = (ct >> 4) ^ ct;
-        ct = (ct >> 2) ^ ct;
-        ct = (ct >> 1) ^ ct;
-        if((ct & 1) == 0) { // если соотношение чётное, то поменять первый бит
-            x = x ^ 1;
-        } else { // иначе поменять бит сразу выше последней 1
-            int y = x ^ (x & (x - 1)); // сначала вычислить последнюю 1
-            y = (y << 1) & bits.getMask();
-            x = (y == 0 ? 0 : x ^ y);
+    @Override
+    public void propagate(InstanceState state) {
+        ProcessorClockState data = (ProcessorClockState) state.getData();
+        if (data == null) {
+            data = new ProcessorClockState();
+            state.setData(data);
         }
-        return Value.createKnown(bits, x);
+
+        Object irqTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
+        boolean irqTriggered = data.updateClock(state.getPort(7), irqTriggerType, Processor.ClockType.IRQ);
+
+        Object excTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
+        boolean excTriggered = data.updateClock(state.getPort(9), excTriggerType, Processor.ClockType.EXC);
+
+        Object clkTriggerType = state.getAttributeValue(StdAttr.TRIGGER);
+        boolean clkTriggered = data.updateClock(state.getPort(10), clkTriggerType, Processor.ClockType.CLK);
+
+
+        if (irqTriggered){
+            externalInterrupt(state.getPort(8).toIntValue());
+        }
+        if (excTriggered){
+            externalException(state.getPort(11).toIntValue());
+        }
+        if (clkTriggered){
+            clockRising();
+        }
+        else {
+            clockFalling();
+        }
+
+        update();
     }
 }
