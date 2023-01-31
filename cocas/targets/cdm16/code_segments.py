0a115925a0ac9ccacbff52bc49f7c2a4ff272ef5
Timofey X <vuki03@mail.ru>
2023-02-01 01:30:30 +0700

    [cocas] Working ldi

--- cocas/targets/cdm16/code_segments.py
+++ cocas/targets/cdm16/code_segments.py
@@ -1,6 +1,22 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
+import bitstruct
+
+from cocas.ast_nodes import RelocatableExpressionNode, LabelNode, TemplateFieldNode, RegisterNode
+from cocas.code_block import Section
 from cocas.default_code_segments import CodeSegmentsInterface
+from cocas.error import CdmException, CdmExceptionTag
+from cocas.location import CodeLocation
+from cocas.object_module import ObjectSectionRecord, ExternalEntry
+
+
+def pack(fmt, *args):
+    b = bitstruct.pack(fmt, *args)
+    return bitstruct.byteswap('2', b)
+
+
+def _error(segment: CodeSegmentsInterface.CodeSegment, message: str):
+    raise CdmException(CdmExceptionTag.ASM, segment.location.file, segment.location.line, message)
 
 
 class CodeSegments(CodeSegmentsInterface):
@@ -8,9 +24,87 @@ class CodeSegments(CodeSegmentsInterface):
     class CodeSegment(CodeSegmentsInterface.CodeSegment):
         pass
 
+    class BytesSegment(CodeSegment):
+        data: bytearray
+
+        def __init__(self, data: bytearray):
+            self.data = data
+            self.size = len(data)
+
+        def fill(self, object_record: ObjectSectionRecord, section: Section, labels: dict[str, int],
+                 templates: dict[str, dict[str, int]]):
+            object_record.data += self.data
+
     class VaryingLengthSegment(CodeSegment, CodeSegmentsInterface.VaryingLengthSegment):
         pass
+
+    class LdiSegment(CodeSegment):
+        expr: RelocatableExpressionNode
+
+        def __init__(self, register: RegisterNode, expr: RelocatableExpressionNode):
+            self.reg: int = register.number
+            self.expr = expr
+            self.size = 4
+            self.location: CodeLocation = CodeLocation()
+
+        def fill(self, object_record: ObjectSectionRecord, section: Section, labels: dict[str, int],
+                 templates: dict[str, dict[str, int]]):
+            parsed = CodeSegments.parse_expression(self.expr, section, labels, templates, self)
+            if parsed.external or parsed.relative_additions != 0 or not -64 <= parsed.value_with_relative < 64:
+                object_record.data.extend(pack("u3p6u4u3", 0b001, 2, self.reg))
+                offset = section.address + len(object_record.data)
+                object_record.data.extend((parsed.value_with_relative % 65536).to_bytes(2, 'little'))
+                for label in parsed.external:
+                    if parsed.external[label] != 0:
+                        entry = object_record.external.setdefault(label, [])
+                        n = abs(parsed.external[label])
+                        sign = parsed.external[label] // n
+                        for i in range(n):
+                            entry.append(ExternalEntry(offset, range(0, 2), sign))
+                if parsed.relative_additions != 0:
+                    sign = parsed.relative_additions // abs(parsed.relative_additions)
+                    for i in range(abs(parsed.relative_additions)):
+                        object_record.relative.append(ExternalEntry(offset, range(0, 2), sign))
+            else:
+                object_record.data.extend(pack("u3u3s7u3", 0b011, 5, parsed.value_with_relative, self.reg))
+                self.size = 2
 
     @dataclass
     class BytesSegment(CodeSegment):
         data: bytearray
+
+    @dataclass
+    class ParsedExpression:
+        value: int
+        value_with_relative: int = field(init=False)
+        relative_additions: int = field(default=0)
+        relative: dict[str, int] = field(default_factory=dict)
+        external: dict[str, int] = field(default_factory=dict)
+
+    @staticmethod
+    def parse_expression(expr: RelocatableExpressionNode, s: Section, asect_labels: dict[str, int],
+                         templates: dict[str, dict[str, int]], segment: CodeSegment):
+        if expr.byte_specifier is not None:
+            _error(segment, f'No byte specifiers allowed in CdM-16')
+        result = CodeSegments.ParsedExpression(expr.const_term)
+        for term, sign in [(t, 1) for t in expr.add_terms] + [(t, -1) for t in expr.sub_terms]:
+            if isinstance(term, LabelNode):
+                if term.name in asect_labels:
+                    result.value += asect_labels[term.name] * sign
+                elif term.name in s.labels:
+                    result.relative[term.name] = result.relative.get(term.name, 0) + sign
+                elif term.name in s.exts:
+                    result.external[term.name] = result.external.get(term.name, 0) + sign
+                else:
+                    _error(segment, f'Label "{term.name}" not found')
+            elif isinstance(term, TemplateFieldNode):
+                result.value += templates[term.template_name][term.field_name] * sign
+        # result.relative = {label: n for label, n in result.relative.items() if n != 0}
+        # result.external = {label: n for label, n in result.external.items() if n != 0}
+
+        result.value_with_relative = result.value
+        for label, n in result.relative.items():
+            rel_address = s.labels[label]
+            result.value_with_relative += rel_address * n
+            result.relative_additions += n
+        return result
--- cocas/targets/cdm16/target_instructions.py
+++ cocas/targets/cdm16/target_instructions.py
@@ -1,10 +1,43 @@
-from cocas.ast_nodes import InstructionNode
+from typing import get_origin, get_args, Union
+
+from cocas.ast_nodes import InstructionNode, RegisterNode, RelocatableExpressionNode
 from cocas.default_code_segments import CodeSegmentsInterface
 from cocas.default_instructions import TargetInstructionsInterface
+from cocas.error import CdmTempException, CdmException, CdmExceptionTag
+from .code_segments import CodeSegments
+
+
+def assert_args(args, *types):
+    ts = [((t,) if get_origin(t) is None else get_args(t)) for t in types]
+    for i in range(len(args)):
+        for j in ts[i]:
+            if not isinstance(args[i], j):
+                raise CdmTempException(f'Incompatible argument type {type(args[i])}')
+        if isinstance(args[i], RegisterNode) and not 0 <= args[i].number <= 7:
+            raise CdmTempException(f'Invalid register number r{args[i].number}')
 
 
 class TargetInstructions(TargetInstructionsInterface):
     @staticmethod
     def assemble_instruction(line: InstructionNode, temp_storage) \
             -> list[CodeSegmentsInterface.CodeSegment]:
-        return []
+        try:
+            if line.mnemonic == 'ldi':
+                args = line.arguments
+                if len(args) == 2:
+                    assert_args(args, RegisterNode, RelocatableExpressionNode)
+                    return [CodeSegments.LdiSegment(*args)]
+                else:
+                    raise CdmTempException(f'Expected 2 arguments, found {len(args)}')
+            else:
+                exit(-1)
+        except CdmTempException as e:
+            raise CdmException(CdmExceptionTag.ASM, line.location.file, line.location.line, e.message)
+
+# if len(line.arguments) == 2:
+#     assert_args(line.arguments, RegisterNode, Union[RegisterNode, RelocatableExpressionNode])
+#     return
+# elif len(line.arguments) == 3:
+#     assert_args(line.arguments, RegisterNode, RegisterNode, RegisterNode)
+# else:
+#     raise CdmTempException(f'Expected 2 or 3 arguments, but found {len(line.arguments)}')
