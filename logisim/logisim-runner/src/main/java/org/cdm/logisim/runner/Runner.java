c4609dbe674e9ba3a989f8e312cd62c2fae9944a
Nikolay Repin <nikolat-repin201303@mail.ru>
2023-03-27 00:17:36 +0700

    [logisim-runner] Refactor code
    + Move main() to separate class
    + Rewrite error handling
    + Add ticks count and image size to output JSON

--- logisim/logisim-runner/src/main/java/org/cdm/logisim/runner/Runner.java
+++ logisim/logisim-runner/src/main/java/org/cdm/logisim/runner/Runner.java
@@ -14,19 +14,14 @@ import com.cburch.logisim.instance.InstanceState;
 import com.cburch.logisim.proj.Project;
 import com.cburch.logisim.std.memory.Rom;
 import com.cburch.logisim.std.wiring.Pin;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import org.cdm.logisim.memory.BankedMemState;
 import org.cdm.logisim.memory.BankedRAM;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -36,73 +31,20 @@ import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class Runner {
-    private static final int IMG_FILE = 0;
-    private static final int CIRCUIT_FILE = 1;
-    private static final int OUTPUT_FILE = 2;
-    private static final int CONFIG_FILE = 3;
-    private static final int TIMEOUT = 4;
 
-    public Runner() {
-    }
+    public RunnerState run(
+            File inputFile,
+            File circuitFile,
+            File configFile,
+            int timeout)
+            throws RunnerException, TimeoutException {
 
-    public static void main(String[] args) {
-        Runner runner = new Runner();
-        File imgFile = new File(args[IMG_FILE]);
-        File circuitFile = new File(args[CIRCUIT_FILE]);
-        File configFile = new File(args[CONFIG_FILE]);
-        if (!imgFile.exists()) {
-            System.err.println("Cannot find image file");
-            System.exit(1);
-        } else if (!circuitFile.exists()) {
-            System.err.println("Cannot find circuit file");
-            System.exit(1);
-        } else if (!configFile.exists()) {
-            System.err.println("Cannot find config file");
-            System.exit(1);
-        } else if (!(new File(args[OUTPUT_FILE])).exists()) {
-            System.err.println("Cannot find output file");
-            System.exit(1);
-        }
-
-        OutputStreamWriter outputStreamWriter = null;
-
-        try {
-            outputStreamWriter = new OutputStreamWriter(
-                    new FileOutputStream(args[OUTPUT_FILE]),
-                    StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            System.err.println("Failed to open OutputFile");
-            System.exit(1);
-        }
-
-        int timeout = Integer.parseInt(args[TIMEOUT]);
-        JsonElement res = runner.run(imgFile, circuitFile, configFile, timeout);
-
-        try {
-            System.out.println(res);
-            outputStreamWriter.write(res.toString());
-            outputStreamWriter.close();
-        } catch (IOException e) {
-            System.err.println("Failed to write to output file");
-            System.exit(1);
-        }
-
-        System.exit(0);
-    }
-
-    private int getPinValue(CircuitState circuitState, Instance pin) {
-        InstanceState state = circuitState.getInstanceState(pin);
-        return Pin.FACTORY.getValue(state).toIntValue();
-    }
-
-    public JsonElement run(File inputFile, File circuitFile, File configFile, int timeout) {
         Properties configProperties = new Properties();
 
         try {
             configProperties.load(new FileInputStream(configFile));
         } catch (IllegalArgumentException | IOException var27) {
-            System.err.println("Cannot find config file");
-            System.exit(1);
+            throw new RunnerException("Cannot find config file");
         }
 
         List<String> pin_names = Arrays.stream(
@@ -111,22 +53,20 @@ public class Runner {
                 .toList();
         String halt_pin = configProperties.getProperty("halt_pin");
         Loader logisimLoader = new Loader(null);
-        InputStream stream = null;
+        InputStream stream;
 
         try {
             stream = new FileInputStream(circuitFile);
         } catch (FileNotFoundException e) {
-            System.err.println("Cannot find circuit file");
-            System.exit(1);
+            throw new RunnerException("Cannot find circuit file");
         }
 
-        LogisimFile logisimFile = null;
+        LogisimFile logisimFile;
 
         try {
             logisimFile = logisimLoader.openLogisimFile(stream);
         } catch (IOException | LoadFailedException e) {
-            System.err.println("unable to open logisim file");
-            System.exit(1);
+            throw new RunnerException("Unable to open Logisim file");
         }
 
         Project logisimProject = new Project(logisimFile);
@@ -139,7 +79,7 @@ public class Runner {
                 .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
         Instance haltPin = outputPins.get(halt_pin);
         if (haltPin == null) {
-            throw new NullPointerException();
+            throw new RunnerException("Unable to find halt pin");
         } else {
             CircuitState circuitState = new CircuitState(logisimProject, circuit);
             Propagator propagator = circuitState.getPropagator();
@@ -150,8 +90,7 @@ public class Runner {
                     .findFirst()
                     .orElse(null);
             if (romComponent == null) {
-                System.err.println("there is no ROM found");
-                System.exit(1);
+                throw new RunnerException("Unable to find ROM");
             }
 
             propagator.propagate();
@@ -160,8 +99,7 @@ public class Runner {
                 ((Rom) romComponent.getFactory())
                         .loadImage(circuitState.getInstanceState(romComponent), inputFile);
             } catch (IOException e) {
-                System.err.println("Failed to load image");
-                System.exit(1);
+                throw new RunnerException("Failed to load image");
             }
 
             propagator.propagate();
@@ -173,8 +111,7 @@ public class Runner {
             }
 
             if (ticks == timeout) {
-                System.err.println("Timeout");
-                System.exit(13);
+                throw new TimeoutException();
             }
 
             Map<String, Integer> registersValues = new HashMap<>();
@@ -182,21 +119,23 @@ public class Runner {
                 registersValues.put(pin, getPinValue(circuitState, outputPins.get(pin)));
             }
 
-            HexModel contents = this.altGetRamContents(circuit, circuitState);
-            List<Integer> r = new ArrayList<>();
+            HexModel contents = this.getRamContents(circuit, circuitState);
+            List<Integer> memoryContent = new ArrayList<>();
 
             for (long i = 0L; i < contents.getLastOffset(); ++i) {
-                r.add(contents.get(i));
+                memoryContent.add(contents.get(i));
             }
 
-            Gson gson = new Gson();
-            JsonElement g = gson.toJsonTree(registersValues);
-            g.getAsJsonObject().add("mem", gson.toJsonTree(r));
-            return g;
+            return new RunnerState(registersValues, memoryContent, ticks);
         }
     }
 
-    private HexModel altGetRamContents(Circuit circuit, CircuitState circuitState) {
+    private int getPinValue(CircuitState circuitState, Instance pin) {
+        InstanceState state = circuitState.getInstanceState(pin);
+        return Pin.FACTORY.getValue(state).toIntValue();
+    }
+
+    private HexModel getRamContents(Circuit circuit, CircuitState circuitState) {
         Component ramComponent = circuit
                 .getNonWires()
                 .stream()
