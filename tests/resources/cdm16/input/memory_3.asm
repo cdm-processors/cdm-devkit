618abde32fc8a73ddd4fedc338af79afdf2bcc23
Nikolay Repin <nikolat-repin201303@mail.ru>
2023-03-29 14:49:45 +0700

    [tests] Add some tests for CdM-16

--- tests/resources/cdm16/input/memory_3.asm
+++ tests/resources/cdm16/input/memory_3.asm
@@ -5,11 +5,13 @@ default_handler: ext    # as external
 # Interrupt vector table (IVT)
 # Place a vector to program start and
 # map all internal exceptions to default_handler
-dc main, 0x8000              # Startup/Reset vector
+dc main, 0x0000         # Startup/Reset vector
 dc default_handler, 0   # Unaligned SP
 dc default_handler, 0   # Unaligned PC
 dc default_handler, 0   # Invalid instruction
 dc default_handler, 0   # Division by zero
+dc 0, 0                 # Reserved
+dc default_handler, 0   # Interrupt fault
 align 0x80              # Reserve space for the rest
                         # of IVT
 
@@ -22,7 +24,38 @@ default_handler>
 
 # Main program section
 rsect main
+data: ext
 main>
+    ldi r0, 0x8000
+    ldi r1, 0xaabb
+
+    ldi r2, -2
+    ldi r3, -3
+
+    stw r0, r2, r1
+    stb r0, r3, r1
+
+    ldw r0, r2, r4
+    ldb r0, r2, r5
+    ldsb r0, r2, r6
+
+    ldi r0, data
+    ldi r1, 2
+
+    lcb r0, r1, r2
+    lcsb r0, r1, r3
+    lcw r0, r1, r7
+
+    ldi r1, 1
+
+    lcw r0, r1, r1
+
+    lcw r0, r0
+
     halt
+
+rsect data
+data>
+    dw 0xbbaa, 0xfa86
 
 end.
--- tests/resources/cdm16/input/reg_transfer.asm
+++ tests/resources/cdm16/input/reg_transfer.asm
@@ -5,11 +5,13 @@ default_handler: ext    # as external
 # Interrupt vector table (IVT)
 # Place a vector to program start and
 # map all internal exceptions to default_handler
-dc main, 0x8000              # Startup/Reset vector
+dc main, 0x8765         # Startup/Reset vector
 dc default_handler, 0   # Unaligned SP
 dc default_handler, 0   # Unaligned PC
 dc default_handler, 0   # Invalid instruction
 dc default_handler, 0   # Division by zero
+dc 0, 0                 # Reserved
+dc default_handler, 0   # Interrupt fault
 align 0x80              # Reserve space for the rest
                         # of IVT
 
@@ -23,6 +25,25 @@ default_handler>
 # Main program section
 rsect main
 main>
+    ldi r0, 1
+    ldi r1, -1
+    ldi r2, 0xaab0
+    ldi fp, 64
+
+    stsp r2
+    ldsp r4
+
+    ldps r6
+
+    move r7, r3
+
+    stps r1
+
+    stpc r2
+
+asect 0xaab0
+    ldpc r5
+
     halt
 
 end.
