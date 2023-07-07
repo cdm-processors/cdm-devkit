99bd7e6ebc8f38ae4aa2b805dd3c67c71108954e
Nikolay Repin <nikolat-repin201303@mail.ru>
2023-07-07 19:36:52 +0700

    [cocodump] Implement cdm8 and cdm8e targets

---
+++ cocodump/targets/cdm8/asm.py
@@ -0,0 +1,45 @@
+from enum import IntEnum
+
+from cocodump.base_types import InstructionDecoder
+
+
+class InstructionGroup(IntEnum):
+    DIRECT = 0x0
+    UNARY_ARITH = 0x8
+    SHIFTS = 0x9
+    ST = 0xA
+    LD = 0xB
+    PUSH_GR = 0xC
+    LDI_GR = 0xD
+    BRANCHES = 0xE
+    LDC = 0xF
+    SP_GROUP = 0xCC
+
+
+inst_decoders: dict[InstructionGroup | int, InstructionDecoder] = {
+    InstructionGroup.DIRECT: InstructionDecoder({0: "move", 1: "add", 2: "addc", 3: "sub", 4: "and",
+                                                 5: "or", 6: "xor", 7: "cmp"}),
+
+    InstructionGroup.UNARY_ARITH: InstructionDecoder({0: "not", 1: "neg", 2: "dec", 3: "inc"}),
+
+    InstructionGroup.SHIFTS: InstructionDecoder({0: "shr", 1: "shl", 2: "shra", 3: "rol"}),
+
+    InstructionGroup.PUSH_GR: InstructionDecoder({0: "push", 1: "pop", 2: "ldsa"}),
+
+    InstructionGroup.SP_GROUP: InstructionDecoder({0: "addsp", 1: "setsp", 2: "pushall", 3: "popall"}),
+
+    InstructionGroup.LDI_GR: InstructionDecoder({4: "halt", 5: "wait", 6: "jsr", 7: "rts",
+                                                 8: "ioi", 9: "rti", 10: "crc", 11: "osix"}),
+
+    InstructionGroup.BRANCHES: InstructionDecoder({0: "beq", 1: "bne", 2: "bhs", 3: "blo", 4: "bmi", 5: "bpl",
+                                                   6: "bvs", 7: "bvc", 8: "bhi", 9: "bls", 10: "bge", 11: "blt",
+                                                   12: "bgt", 13: "ble", 14: "br", 15: "nop"})
+
+}
+
+registers = {
+    0: "r0",
+    1: "r1",
+    2: "r2",
+    3: "r3"
+}
---
+++ cocodump/targets/cdm8e/decoder.py
@@ -0,0 +1,74 @@
+from cocodump.base_types import BranchInstruction, DecodedSection
+from cocodump.targets.cdm8.asm import InstructionGroup, inst_decoders
+from cocodump.targets.cdm8.decoder import decode_inst
+
+
+def normalize_imm8(val: int):
+    if val >= 0x80:
+        return (-1 << 8) | val
+    else:
+        return val
+
+
+def decode(image: bytearray) -> DecodedSection:
+    inst_decoders[InstructionGroup.LDI_GR].mnemonics[13] = "jmp"
+
+    section = DecodedSection()
+
+    section_iter = iter(image)
+    current_addr = 0
+
+    while True:
+        try:
+            byte = next(section_iter)
+
+            (inst, inst_gr) = decode_inst(byte)
+
+            if inst.inst == "jmp":
+                inst = BranchInstruction(
+                    "jmp",
+                    []
+                )
+
+            inst.addr = current_addr
+            inst.inst_bytes = [byte]
+
+            current_addr += 1
+
+            if inst.inst in ["jsr", "jmp"]:
+                lower_byte = next(section_iter)
+                higher_byte = next(section_iter)
+
+                inst.inst_bytes.append(lower_byte)
+                inst.inst_bytes.append(higher_byte)
+
+                word = (higher_byte << 8) | lower_byte
+
+                inst.br_addr = word
+                inst.args.append(hex(word))
+
+                current_addr += 2
+
+            if inst_gr == InstructionGroup.BRANCHES or \
+                    inst.inst in ["ldsa", "addsp", "setsp", "ldi", "osix"]:
+                byte = next(section_iter)
+
+                inst.inst_bytes.append(byte)
+
+                current_addr += 1
+
+                # TODO: Refactor
+                if inst_gr == InstructionGroup.BRANCHES:
+                    inst: BranchInstruction
+
+                    inst.br_addr = current_addr + normalize_imm8(byte) - 1
+                    inst.args.append(str(normalize_imm8(byte)) if byte >= 0x80 else '+' + str(byte))
+                else:
+                    inst.args.append(hex(byte))
+
+            section.add_inst(inst)
+
+        except StopIteration:
+            break
+
+    return section
