b7abf884654e6eae8fb36cbb41a45f1e55c0c8a1
Melnikov Andrey <a.melnikov4@g.nsu.ru>
2023-10-04 01:33:34 +0700

    [tests] Remove zeros from .yml test file

--- tests/resources/cdm16/input/alu_flags.asm
+++ tests/resources/cdm16/input/alu_flags.asm
@@ -31,115 +31,275 @@ main>
     # Ordinary test case
     ldi r1, 15
     ldi r2, 16
+    ldi r0, 31
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Zero test case
     ldi r1, 15
     ldi r2, -15
+    ldi r0, 0
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Overflow (negative overflow) test case
     ldi r1, 32760
     ldi r2, 65
+    ldi r0, 32825
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 0
+    fi
 
     # Carry (unsigned overflow) test case
     ldi r1, 65530
     ldi r2, 65
+    ldi r0, 59
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Just zero test case
     ldi r1, 0
     ldi r2, 0
+    ldi r0, 0
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Just negative case
     ldi r1, 0
     ldi r2, -5
+    ldi r0, -5
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Negative plus negative case
     ldi r1, -1
     ldi r2, -1
+    ldi r0, -2
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Negative overflow case
     ldi r1, -32760
     ldi r2, -65
+    ldi r0, 32711
+
     add r2, r1
-    pups
-    push r1
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r1, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # ========================= Sub tests =======================
     # Ordinary test case
     ldi r1, 32
     ldi r2, 13
+    ldi r0, 19
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Zero test case
     ldi r1, 15
     ldi r2, 15
+    ldi r0, 0
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Overflow (negative overflow) test case
     ldi r1, 32760
     ldi r2, -65
+    ldi r0, 32825
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Carry (unsigned overflow) test case
     ldi r1, 65530
     ldi r2, -65
+    ldi r0, 59
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Just zero test case
     ldi r1, 0
     ldi r2, 0
+    ldi r0, 0
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Just negative case
     ldi r1, 0
     ldi r2, 5
-    sub r1, r2
-    pups
-    push r2
+    ldi r0, -5
 
-    # Negative plus negative case
+    sub r1, r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
+
+    # Negative minus positive case
     ldi r1, -1
     ldi r2, 1
+    ldi r0, -2
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # Negative overflow case
     ldi r1, -32760
     ldi r2, 65
+    ldi r0, 32711
+
     sub r1, r2
-    pups
-    push r2
+    ldps r3
+    add r3, 16
+    push r3
+    if
+        cmp r2, r0
+    is eq
+        push 1
+    else
+        push 2
+    fi
 
     # stps r0
     # and r2, r1
