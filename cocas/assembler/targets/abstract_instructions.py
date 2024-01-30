b7234a7372c926fe812e99bd14f38b7f7f8055a7
Timofey X <vuki03@mail.ru>
2024-01-30 23:51:36 +0700

    [cocas] Change target instructions interface to be with abstract methods

--- cocas/assembler/targets/abstract_instructions.py
+++ cocas/assembler/targets/abstract_instructions.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 from cocas.object_module import CodeLocation
@@ -8,21 +9,25 @@ if TYPE_CHECKING:
     from cocas.assembler.ast_nodes import InstructionNode
 
 
-class TargetInstructionsInterface:
+class TargetInstructionsInterface(ABC):
     @staticmethod
+    @abstractmethod
     def assemble_instruction(line: "InstructionNode", temp_storage) \
             -> list[ICodeSegment]:
         pass
 
     @staticmethod
+    @abstractmethod
     def finish(temp_storage: dict):
         return
 
     @staticmethod
+    @abstractmethod
     def make_branch_instruction(location: CodeLocation, branch_mnemonic: str, label_name: str, inverse: bool) \
             -> list[ICodeSegment]:
         pass
 
     @staticmethod
+    @abstractmethod
     def assembly_directives() -> set[str]:
         pass
