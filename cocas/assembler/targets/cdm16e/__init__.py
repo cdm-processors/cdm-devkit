5bd127b033b8e32e45ce4e89146b8bd83cf795df
Timofey X <vuki03@mail.ru>
2024-02-25 18:52:33 +0700

    [cocas] Re-add cdm16e support to assembler

---
+++ cocas/assembler/targets/cdm16e/__init__.py
@@ -0,0 +1,1 @@
+from .target_instructions import assemble_instruction, assembly_directives, finish, make_branch_instruction
--- cocas/targets/cdm16e/code_segments.py
+++
@@ -1,5 +0,0 @@
-from ..cdm16 import code_segments
-
-
-class CodeSegments(code_segments.CodeSegments):
-    pass
