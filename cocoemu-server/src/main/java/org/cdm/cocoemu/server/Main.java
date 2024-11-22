06992cf7bebf1419f6cfc411a015c2101d800db3
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-11-23 01:14:40 +0700

    [cocoemu-server] Use existing Server and delete new Server.java

--- cocoemu-server/src/main/java/org/cdm/cocoemu/server/Main.java
+++ cocoemu-server/src/main/java/org/cdm/cocoemu/server/Main.java
@@ -1,11 +1,26 @@
 package org.cdm.cocoemu.server;
 
+import org.cdm.cocoemu.server.app.Emulator;
+import org.cdm.cocoemu.server.debug.ServerMessageHandler;
+import org.cdm.debug.MessageHandler;
+import org.cdm.debug.server.Server;
+
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
             throw new IllegalArgumentException("Expected exactly one argument");
         }
         int port = Integer.parseInt(args[0]);
+        Emulator e = new Emulator();
+        Supplier<MessageHandler> s = new Supplier<MessageHandler>() {
+            @Override
+            public ServerMessageHandler get() {
+                return new ServerMessageHandler(e);
+            }
+        };
+        Server server = new Server(port, s);
 
     }
 }
