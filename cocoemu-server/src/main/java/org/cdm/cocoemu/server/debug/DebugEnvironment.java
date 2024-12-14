8058d0458a0b3900293963ead24211ef2626fd8c
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-12-14 14:26:26 +0700

    [cocoemu-server] Remove emulator abstraction, and add Lombok

--- cocoemu-server/src/main/java/org/cdm/cocoemu/server/debug/DebugEnvironment.java
+++ cocoemu-server/src/main/java/org/cdm/cocoemu/server/debug/DebugEnvironment.java
@@ -1,21 +1,15 @@
 package org.cdm.cocoemu.server.debug;
 
+import lombok.Getter;
+import org.cdm.cocoemu.core.Component;
 import org.cdm.cocoemu.server.adapter.ProcessorAdapter;
-import org.cdm.cocoemu.server.emulator.CdmEmulator;
 
+@Getter
 public class DebugEnvironment<T> {
     private final ProcessorAdapter<T> processorAdapter;
-    private final CdmEmulator<T> emulator;
-    protected DebugEnvironment(ProcessorAdapter<T> processorAdapter, CdmEmulator<T> emulator) {
+    private final Component processor;
+    protected DebugEnvironment(ProcessorAdapter<T> processorAdapter, Component processor) {
         this.processorAdapter = processorAdapter;
-        this.emulator = emulator;
-    }
-
-    public ProcessorAdapter<T> getProcessorAdapter() {
-        return processorAdapter;
-    }
-
-    public CdmEmulator<T> getEmulator() {
-        return emulator;
+        this.processor = processor;
     }
 }
