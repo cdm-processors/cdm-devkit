95b4076bfd5e54185cb6531dbb50adb15bc34269
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-12-17 10:27:26 +0700

    [cocoemu-server] Move Lombok to plugins and fix JCommander

--- cocoemu-server/src/main/java/org/cdm/cocoemu/server/Main.java
+++ cocoemu-server/src/main/java/org/cdm/cocoemu/server/Main.java
@@ -1,19 +1,16 @@
 package org.cdm.cocoemu.server;
 
 import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import org.cdm.cocoemu.server.debug.ServerMessageHandler;
 import org.cdm.debug.server.Server;
 
 public class Main {
-    @Parameter(names = {"--port", "-p"})
-    private int port = 7001;
+    public static void main(String ... argv) {
+        Args args = new Args();
+        JCommander p = JCommander.newBuilder().addObject(args).build();
+        p.parse(argv);
 
-    public static void main(String ... args) {
-        Main main = new Main();
-//        JCommander.newBuilder().addObject(main).build().parse(args);
-
-        Server server = new Server(main.port, ServerMessageHandler::new);
-
+        Server server = new Server(args.getPort(), ServerMessageHandler::new);
+        server.start();
     }
 }
