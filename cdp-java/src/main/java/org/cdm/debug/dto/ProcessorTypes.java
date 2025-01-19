77dcc7df1d5880683c381c62a1ce388ee7f6c7c5
Nikolay Repin <nikolat-repin201303@mail.ru>
2025-01-20 02:05:28 +0700

    [cdp-java] Add literals for processor types

---
+++ cdp-java/src/main/java/org/cdm/debug/dto/ProcessorTypes.java
@@ -0,0 +1,8 @@
+package org.cdm.debug.dto;
+
+public class ProcessorTypes {
+    public static final String CDM8 = "cdm8";
+    public static final String CDM8E = "cdm8e";
+    public static final String CDM16 = "cdm16";
+    public static final String CDM16E = "cdm16e";
+}
