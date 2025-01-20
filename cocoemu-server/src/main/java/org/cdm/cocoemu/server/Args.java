03b50bfe080d0cf2de5bf6b39f95b7bc5992db2e
Nikolay Repin <nikolat-repin201303@mail.ru>
2025-01-21 01:27:29 +0700

    [cocoemu-server] Improve CLI argument parsing

--- cocoemu-server/src/main/java/org/cdm/cocoemu/server/Args.java
+++ cocoemu-server/src/main/java/org/cdm/cocoemu/server/Args.java
@@ -3,17 +3,11 @@ package org.cdm.cocoemu.server;
 import com.beust.jcommander.Parameter;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 public class Args {
-    @Parameter
-    private List<String> args = new ArrayList<>();
-
-    @Parameter(names = {"--port"})
+    @Parameter(names = {"--port", "-p"})
     private Integer port = 7001;
 
-    @Parameter(names = {"--ip"})
-    private String ip = "127.0.0.1";
+    @Parameter(names = {"--help", "-h"}, help = true)
+    public boolean help = false;
 }
