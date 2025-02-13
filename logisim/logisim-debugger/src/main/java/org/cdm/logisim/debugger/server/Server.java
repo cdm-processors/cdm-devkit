06bd23e43ec3b5317519f8f93bc4baa00d22374b
Nikolay Repin <nikolat-repin201303@mail.ru>
2025-02-13 18:45:26 +0700

    [logisim-debugger] Set reuse address to true

--- logisim/logisim-debugger/src/main/java/org/cdm/logisim/debugger/server/Server.java
+++ logisim/logisim-debugger/src/main/java/org/cdm/logisim/debugger/server/Server.java
@@ -22,6 +22,9 @@ public class Server extends WebSocketServer {
 
     public Server(int port) {
         super(new InetSocketAddress(port));
+
+        // Fix BindError
+        setReuseAddr(true);
     }
 
     @Override
