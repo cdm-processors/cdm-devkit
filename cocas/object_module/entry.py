d50188dd2361ec1fc4c34c46fa5d90f35c37a4af
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-03-09 20:18:39 +0700

    [cocas] Move Linkage to the top of entry.py
    To appease old Python

--- cocas/object_module/entry.py
+++ cocas/object_module/entry.py
@@ -2,14 +2,19 @@ from dataclasses import dataclass
 from enum import IntEnum
 
 
-@dataclass(frozen=True)
-class Entry:
-    "A symbol entry in a section of an object module."
-    address: int
-    "The offset from the start of the section this entry is defined at."
-
-    def __str__(self):
-        return f"{self.address:02x}"
+class Linkage(IntEnum):
+    "Describes the way an entry or external is linked."
+    FILE_LOCAL = 1
+    "The entry is visible in the current object module only."
+    GLOBAL = 2
+    "The entry is visible in all object modules."
+    WEAK_GLOBAL = 3
+    """
+    The entry is visible in all object modules.
+    When applied to an entry, it makes it possible to override with a GLOBAL entry.
+    When applied to an external, it becomes optional (e.g. doesn't throw an error
+    when there is no matching entry, leaving the value as is.
+    """
 
 
 @dataclass(frozen=True)
@@ -32,16 +37,11 @@ class EntryKey:
                 return self.name
 
 
-class Linkage(IntEnum):
-    "Describes the way an entry or external is linked."
-    FILE_LOCAL = 1
-    "The entry is visible in the current object module only."
-    GLOBAL = 2
-    "The entry is visible in all object modules."
-    WEAK_GLOBAL = 3
-    """
-    The entry is visible in all object modules.
-    When applied to an entry, it makes it possible to override with a GLOBAL entry.
-    When applied to an external, it becomes optional (e.g. doesn't throw an error
-    when there is no matching entry, leaving the value as is.
-    """
+@dataclass(frozen=True)
+class Entry:
+    "A symbol entry in a section of an object module."
+    address: int
+    "The offset from the start of the section this entry is defined at."
+
+    def __str__(self):
+        return f"{self.address:02x}"
